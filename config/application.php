e92ca12ab24147f9d19bfba6984bf896254d5118
Uriah Galang <g0ld3lux@gmail.com>
2017-08-05 15:42:27 +0800

    revert

--- config/application.php
+++ config/application.php
@@ -74,7 +74,7 @@ define('NONCE_SALT', env('NONCE_SALT'));
 define('AUTOMATIC_UPDATER_DISABLED', true);
 define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 define('DISALLOW_FILE_EDIT', true);
-define('FS_METHOD',env('FS_METHOD') ?: false);
+define('FS_METHOD', 'direct');
 
 /**
  * Bootstrap WordPress
